21041339791bfc42adc2e3da8f9cd452a152d30d
华江锋 <BOB\rgzndev066>
2022-04-22 09:58:44 +0800

    生产环境去掉页面上的vconsole显示组件

--- src/main.js
+++ src/main.js
@@ -17,9 +17,9 @@ import './assets/fonts/iconfont.css'
 import 'vant/lib/index.less';
 import store from './store/index.js'
 import './core/utils/tools'
-//import Components from './components/index'
+import Components from './components/index'
 //import VConsose from 'vconsole'
-Vue.use(new VConsose())
+//Vue.use(new VConsose())
 Vue.use(Components) // 全局注册组件
 Vue.use(ToastPlugin)
 Vue.use(LoadingPlugin)
