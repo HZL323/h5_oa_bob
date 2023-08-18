d09c4d94a401b5ad6e012a7f8ecddf3801ba96a4
linxiaoyan01 <xiaoyanlin1866@gmail.com>
2023-08-18 09:58:44 +0800

    退回意见必填增加流程名称

--- src/modules/detail_page/index.vue
+++ src/modules/detail_page/index.vue
@@ -1213,8 +1213,8 @@ export default {
 
       if (
         this.currentProcess.processName === "业务数据处理申请流程" ||
-        this.currentProcess.processName === "支行业务数据处理流程" ||
-        this.currentProcess.processName === "分行业务数据处理流程" ||
+        this.currentProcess.processName === "支行业务数据处理申请流程" ||
+        this.currentProcess.processName === "分行业务数据处理申请流程" ||
         this.currentProcess.processName === "总行办公自动化用户维护申请流程" ||
         this.currentProcess.processName === "通用流程" ||
         this.currentProcess.processName === "总分行办公检法查询申请流程" ||
