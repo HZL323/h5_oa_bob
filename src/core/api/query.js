6b72a4c1fb450f2b56261fd979aeabd93ed3fd67
linxiaoyan01 <xiaoyanlin1866@gmail.com>
2023-09-20 10:27:56 +0800

    增加字典项

--- src/core/api/query.js
+++ src/core/api/query.js
@@ -50,7 +50,7 @@ export function queryKeyValueByTypes() {
     // console.log(`参数`,`DOC_PRIORITY,DOC_SECURITY_LEVEL,FILE_TYPE,is_direct,send_method,PRESERVATION_PERIOD,sys_file_position,HOST_DEP,APPLY_CHANGE_TYPE,DOC_TYPE,ARCHIVE_YEAR,OPEN_STATE,RESPON_PERSON,AUDIO_VISUAL_TYPE,TEXT_SOURCE,PRINT_WAY`)
     return ajaxPost(`${URL}/mob/oa/DictServiceContainer/queryKeyValueByTypes`,
         {
-            dictTypes: `DOC_PRIORITY,DOC_SECURITY_LEVEL,FILE_TYPE,is_direct,send_method,PRESERVATION_PERIOD,sys_file_position,HOST_DEP,APPLY_CHANGE_TYPE,DOC_TYPE,ARCHIVE_YEAR,OPEN_STATE,RESPON_PERSON,AUDIO_VISUAL_TYPE,TEXT_SOURCE,PRINT_WAY,BUSINESS_TYPE,BusinessDataType,DemandType,IsEffectClient,IsInvolveInfo,IsClientRequire,EstimateResult,DemandScreen,IsSearchSynDept,testResult,confirmResult,IsMustTest,isDataDelete,`
+            dictTypes: `DOC_PRIORITY,DOC_SECURITY_LEVEL,FILE_TYPE,is_direct,send_method,PRESERVATION_PERIOD,sys_file_position,HOST_DEP,APPLY_CHANGE_TYPE,DOC_TYPE,ARCHIVE_YEAR,OPEN_STATE,RESPON_PERSON,AUDIO_VISUAL_TYPE,TEXT_SOURCE,PRINT_WAY,BUSINESS_TYPE,BusinessDataType,DemandType,IsEffectClient,IsInvolveInfo,IsClientRequire,EstimateResult,DemandScreen,IsSearchSynDept,testResult,confirmResult,IsMustTest,isDataDelete,smsPubType`
         })
 }
 
